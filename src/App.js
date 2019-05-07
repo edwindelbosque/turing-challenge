b6b49a448988be5c2c7cf31da8097b298675edf3
Christie Lynam <christielynam@gmail.com>
2019-05-07 14:34:30 -0600

    Create framework of App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <h1 className='app-title'>Turing Cafe Reservations</h1>
+        <div className='resy-form'>
+
+        </div>
+        <div className='resy-container'>
+          
+        </div>
       </div>
     )
   }
