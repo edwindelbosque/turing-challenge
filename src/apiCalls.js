a074fb9f9c71abf12e7aff2a44b73a22ecafb042
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-17 11:44:29 -0600

    Add .catch to apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,6 +3,7 @@ export const getReservations = () => {
   return fetch('http://localhost:3001/api/v1/reservations')
     .then(response => response.json())
     .then(reservations => reservations)
+    .catch(error => console.warning(error))
 }
 
 export const postReservation = (reservation) => {
@@ -15,6 +16,7 @@ export const postReservation = (reservation) => {
   })
     .then(response => response.json())
     .then(reservation => reservation)
+    .catch(error => console.warning(error))
 }
 
 export const deleteReservation = (id) => {
